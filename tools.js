5d58ea0c75766845c0beec1b1cd01ebf1b87444d
angela-lm <1281125667@qq.com>
2018-06-13 18:24:17 +0800

    first commit

---
+++ tools.js
@@ -0,0 +1,43 @@
+﻿//   封装函数 getByClassName
+ Document.prototype.getByClassName = function(className){
+        var arr = document.getElementsByTagName('*');
+        var allArr = Array.prototype.slice.call(arr,0);
+        var filterArr = [];
+        function dealClass(dom){
+            var reg = /\s+/g;
+            var arrClassName = dom.className.replace(reg,' ').trim();
+            return arrClassName;
+        }
+        allArr.forEach(function(ele,index){
+            var itemClassArr = dealClass(ele).split(' ');
+            console.log(itemClassArr);
+            for(var i = 0; i < itemClassArr.length;i++){
+                if(itemClassArr[i] == className){
+                    filterArr.push(ele);
+                    break;
+                }
+            }
+        })
+        return filterArr;
+    }
+// 深度克隆
+function deepClone(target,origin){
+        var copy;
+        if(origin != null){
+            for(var prop in origin){
+                src = target[prop];
+                copy = origin[prop];
+                if(copy && typeof copy == 'object'){
+                    if(Object.prototype.toString.call(copy) == '[object Object]'){
+                        src = src ? src : {};
+                    }else{
+                        src = src ? src :[];
+                    }
+                    target[prop] = deepClone(src,copy)
+                }else{
+                    target[prop] = copy;
+                }
+            }
+        }
+        return target;
+    }
